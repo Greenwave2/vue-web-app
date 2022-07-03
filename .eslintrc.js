ad31ebe2779568acd02bd746d8624b4ce03d97d2
YJack0000 <jackleagends@gmail.com>
2022-07-03 22:32:52 +0800

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    root: true,
+    env: {
+      node: true
+    },
+    extends: [
+      'plugin:vue/essential', // plugin:vue/recommended
+      '@vue/standard'
+    ],
     parserOptions: {
-        parser: "babel-eslint"
+      parser: 'babel-eslint'
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     }
-};
+  }
