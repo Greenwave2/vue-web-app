866d574a707f676cc9370aee845e5e139b358119
YJack0000 <jackleagends@gmail.com>
2022-07-05 01:41:39 +0800

    add i18n

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    // i18n warning 處理
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
   plugins: [vue()]
 })
