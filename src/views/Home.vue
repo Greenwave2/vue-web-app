363d9967aedb0329515c44f2f5d07e200a11b027
YJack0000 <jackleagends@gmail.com>
2022-07-05 01:40:14 +0800

    create home page

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
+<template>
+    <el-container>
+        <el-header>
+            <el-affix>
+                <el-button type="primary" @click="router.push('/login')"> Login </el-button>
+            </el-affix>
+        </el-header>
+        <el-main>
+            Welcome
+        </el-main>
+    </el-container>
+</template>
